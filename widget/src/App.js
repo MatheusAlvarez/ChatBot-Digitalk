327d8bdc701838d40d84c0b9de71b15b9098f393
Matheus Maia Alvarez <88752578+MatheusAlvarez@users.noreply.github.com>
2022-12-03 15:28:47 -0300

    Update App.js

--- widget/src/App.js
+++ widget/src/App.js
@@ -3,10 +3,7 @@ import WelcomeWidget from './Components/ChatBot/WelcomeWidget'
 
 function App() {
   return (
-    <>
-      <h1>Em nome do senhor Jesus</h1>
       <WelcomeWidget />
-    </>
   );
 }
 
